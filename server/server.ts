ed65f36d8dc7ca737e2788f34491866c08f02fb4
Brad <bradaxelson@gmail.com>
2021-02-27 18:56:03 -0500

    chore: Remove server

--- server/server.ts
+++
@@ -1,8 +0,0 @@
-import { PokemonService } from './pokemon.service.ts';
-
-// get the port option
-const portArg = Deno.args.find((arg) => arg.includes('--port'));
-// pull the value out of the port option if it was supplied
-const port = portArg ? parseInt(portArg.split('--port=')[1]) : undefined;
-// start the pokemon service
-new PokemonService(port).start();
