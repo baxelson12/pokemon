7a72ed7b4369bea1121305cd0634b6a8d6dbaf3b
Brad <bradaxelson@gmail.com>
2021-02-27 22:15:11 -0500

    chore: Hook in dev, store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { MetaReducer, StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { ParamSerializer } from './store/serializers/param.serializer';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+import { reducers } from './store/reducers';
+
+export const metaReducers: MetaReducer<{}>[] = !environment.production
+  ? [storeFreeze]
+  : [];
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production
