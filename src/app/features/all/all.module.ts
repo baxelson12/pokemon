798b5b1f7c2d529edd2fe706350c76d410c6aa78
baxelson12 <bradaxelson@gmail.com>
2021-03-02 18:15:37 -0500

    feat: all

--- src/app/features/all/all.module.ts
+++ src/app/features/all/all.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AllRoutingModule } from './all-routing.module';
 import { AllComponent } from './all.component';
 import { PokemonModule } from '../../core/components/pokemon/pokemon.module';
+import { BadCardModule } from 'src/app/shared/components/card/card.module';
 
 @NgModule({
   declarations: [AllComponent],
-  imports: [CommonModule, AllRoutingModule, PokemonModule]
+  imports: [CommonModule, AllRoutingModule, PokemonModule, BadCardModule]
 })
 export class AllModule {}
