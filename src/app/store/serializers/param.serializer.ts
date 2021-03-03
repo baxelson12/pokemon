d254593e4d3325a870f91a713f21ad9adc1ed985
baxelson12 <bradaxelson@gmail.com>
2021-03-02 19:06:50 -0500

    feat: Upgrade serializer

--- src/app/store/serializers/param.serializer.ts
+++ src/app/store/serializers/param.serializer.ts
@@ -1,21 +1,37 @@
-import { RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Params,
+  RouterStateSnapshot
+} from '@angular/router';
 import { RouterStateSerializer } from '@ngrx/router-store';
 
 import * as RouterReducer from '../reducers/router.reducer';
 
 export class ParamSerializer
   implements RouterStateSerializer<RouterReducer.State> {
-  serialize(routerState: RouterStateSnapshot): RouterReducer.State {
-    // Iterate through tree
-    let route = routerState.root;
+  private traverse(root: ActivatedRouteSnapshot): Params {
+    // The object to be returned
+    let params: Params = {};
+    // The starting point
+    let route = root;
+    // Iterate til we hit the end
     while (route.firstChild) {
       route = route.firstChild;
+      // Collect all params on the way there (if any)
+      params = { ...params, ...route.params };
     }
+    return params;
+  }
+
+  serialize(routerState: RouterStateSnapshot): RouterReducer.State {
+    // Since we want an object, we can just reduce the array.
+    // root.children provides primary and modal outlets
+    const params = routerState.root.children.reduce(
+      (acc, curr) => ({ ...acc, ...this.traverse(curr) }),
+      {}
+    );
     // Full url
     const { url } = routerState;
-    // Parameters
-    const { params } = route;
-
     return { url, params };
   }
 }
