87dfc293744f32291fdb18ab9b57d8a24c224c9f
baxelson12 <bradaxelson@gmail.com>
2021-03-02 23:03:49 -0500

    feat: Detail

--- src/app/features/detail/detail.module.ts
+++ src/app/features/detail/detail.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { DetailRoutingModule } from './detail-routing.module';
 import { DetailComponent } from './detail.component';
-
+import { BadModalModule } from 'src/app/shared/components/modal/modal.module';
+import { BadCardModule } from 'src/app/shared/components/card/card.module';
+import { BadRadarModule } from 'src/app/shared/components/radar/radar.module';
+import { BadButtonModule } from 'src/app/shared/components/button/button.module';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [DetailComponent],
   imports: [
     CommonModule,
-    DetailRoutingModule
+    DetailRoutingModule,
+    BadModalModule,
+    BadCardModule,
+    BadRadarModule,
+    BadButtonModule,
+    AngularSvgIconModule
   ]
 })
-export class DetailModule { }
+export class DetailModule {}
