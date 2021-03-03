b5cdd45d7471d6e253f61221e6eee7eb1c91a78e
baxelson12 <bradaxelson@gmail.com>
2021-03-02 23:04:43 -0500

    fix: style tweaks

--- src/app/core/components/pokemon/pokemon.module.ts
+++ src/app/core/components/pokemon/pokemon.module.ts
@@ -4,10 +4,17 @@ import { PokemonComponent } from './pokemon.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { BadCardModule } from '../../../shared/components/card/card.module';
 import { BadButtonModule } from '../../../shared/components/button/button.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PokemonComponent],
-  imports: [CommonModule, AngularSvgIconModule, BadCardModule, BadButtonModule],
+  imports: [
+    CommonModule,
+    AngularSvgIconModule,
+    BadCardModule,
+    BadButtonModule,
+    RouterModule
+  ],
   exports: [PokemonComponent]
 })
 export class PokemonModule {}
